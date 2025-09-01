bf6c67c9b81761f9cd0efe129d49d09d8f9c5246
dorythecat <pemegomachado@gmail.com>
2025-09-01 15:12:52 +0200

    Improved code

--- src/main.js
+++ src/main.js
@@ -15,13 +15,13 @@ import { Application, Graphics } from "pixi.js";
   circle.eventMode = "static";
   circle.cursor = "pointer";
 
-  circle.on('pointerdown', (event) => {
+  circle.on('pointerdown', () => {
       circle.scale.set(0.5, 0.5);
   }, circle);
 
   app.stage.eventMode = 'static';
   app.stage.hitArea = app.screen;
-  app.stage.on('pointerup', (event) => {
+  app.stage.on('pointerup', () => {
       circle.scale.set(1, 1);
   });
 
