bc4d1e1901f2323e8ff6c9478bd4e2f11756e5e8
dorythecat <pemegomachado@gmail.com>
2025-09-01 11:51:09 +0200

    Made into dragging demo

--- src/main.js
+++ src/main.js
@@ -13,23 +13,67 @@ import { Application, Assets, Sprite } from "pixi.js";
   // Load the bunny texture
   const texture = await Assets.load("/assets/bunny.png");
 
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+    // Set the texture's scale mode to nearest to preserve pixelation
+    texture.source.scaleMode = "nearest";
 
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
+    for (let i = 0; i < 10; i++) {
+        createBunny(Math.floor(Math.random() * app.screen.width), Math.floor(Math.random() * app.screen.height));
+    }
 
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+    function createBunny(x, y) {
+        // Create our little bunny friend..
+        const bunny = new Sprite(texture);
 
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
+        // Enable the bunny to be interactive... this will allow it to respond to mouse and touch events
+        bunny.eventMode = 'static';
 
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
+        // This button mode will mean the hand cursor appears when you roll over the bunny with your mouse
+        bunny.cursor = 'pointer';
+
+        // Center the bunny's anchor point
+        bunny.anchor.set(0.5);
+
+        // Make it a bit bigger, so it's easier to grab
+        bunny.scale.set(3);
+
+        // Setup events for mouse + touch using the pointer events
+        bunny.on('pointerdown', onDragStart, bunny);
+
+        // Move the sprite to its designated position
+        bunny.x = x;
+        bunny.y = y;
+
+        // Add it to the stage
+        app.stage.addChild(bunny);
+    }
+
+    let dragTarget = null;
+
+    app.stage.eventMode = 'static';
+    app.stage.hitArea = app.screen;
+    app.stage.on('pointerup', onDragEnd);
+    app.stage.on('pointerupoutside', onDragEnd);
+
+    function onDragMove(event) {
+        if (dragTarget) {
+            dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+        }
+    }
+
+    function onDragStart() {
+        // Store a reference to the data
+        // * The reason for this is because of multitouch *
+        // * We want to track the movement of this particular touch *
+        this.alpha = 0.5;
+        dragTarget = this;
+        app.stage.on('pointermove', onDragMove);
+    }
+
+    function onDragEnd() {
+        if (dragTarget) {
+            app.stage.off('pointermove', onDragMove);
+            dragTarget.alpha = 1;
+            dragTarget = null;
+        }
+    }
 })();
