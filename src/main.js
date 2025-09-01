841023eb7cb0b2e00c72b84eac48393c2b0f6eeb
dorythecat <pemegomachado@gmail.com>
2025-09-01 16:38:50 +0200

    Abstracted circle creation

--- src/main.js
+++ src/main.js
@@ -10,31 +10,29 @@ import { Application, Graphics } from "pixi.js";
   // Append the application canvas to the document body
   document.getElementById("pixi-container").appendChild(app.canvas);
 
-  const circle = new Graphics().circle(100, 100, 50).fill("#000000");
+  function addCircle(radius, x, y) {
+      const circle = new Graphics().circle(x, y, radius).fill("#000000");
 
-  circle.eventMode = "static";
-  circle.cursor = "pointer";
+      circle.eventMode = "static";
+      circle.cursor = "pointer";
 
-  // Set the pivot so it's in the center of the circle, and translate the circle back to its position
-  circle.pivot.set(circle.width, circle.height);
-  circle.x = circle.x + circle.width / 2;
-  circle.y = circle.y + circle.height / 2;
+      // Set the pivot so it's in the center of the circle, and translate the circle back to its position
+      circle.pivot.set(x, y);
+      circle.x = x
+      circle.y = y
 
-  circle.on('pointerdown', () => {
-      circle.scale.set(0.5, 0.5);
-  }, circle);
+      circle.on('pointerdown', () => {
+          circle.scale.set(0.5, 0.5);
+      });
 
-  app.stage.eventMode = 'static';
-  app.stage.hitArea = app.screen;
-  app.stage.on('pointerup', () => {
-      circle.scale.set(1, 1);
-  });
+      app.stage.eventMode = 'static';
+      app.stage.hitArea = app.screen;
+      app.stage.on('pointerup', () => {
+          circle.scale.set(1, 1);
+      });
 
-  app.stage.addChild(circle);
+      app.stage.addChild(circle);
+  }
 
-  let time = 0;
-  app.ticker.add((ticker) => {
-    time += ticker.deltaTime / 10;
-    circle.alpha = Math.sin(time);
-  });
+  addCircle(50, 100, 100);
 })();
