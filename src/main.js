578bc7e3acd30dc612b751335ca2c9fed689a24e
dorythecat <pemegomachado@gmail.com>
2025-09-01 11:58:10 +0200

    Added circle

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Graphics } from "pixi.js";
 
 (async () => {
   // Create a new application
@@ -10,70 +10,7 @@ import { Application, Assets, Sprite } from "pixi.js";
   // Append the application canvas to the document body
   document.getElementById("pixi-container").appendChild(app.canvas);
 
-  // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  const circle = new Graphics().circle(50, 50, 50).fill("#000000");
 
-    // Set the texture's scale mode to nearest to preserve pixelation
-    texture.source.scaleMode = "nearest";
-
-    for (let i = 0; i < 10; i++) {
-        createBunny(Math.floor(Math.random() * app.screen.width), Math.floor(Math.random() * app.screen.height));
-    }
-
-    function createBunny(x, y) {
-        // Create our little bunny friend..
-        const bunny = new Sprite(texture);
-
-        // Enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-        bunny.eventMode = 'static';
-
-        // This button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-        bunny.cursor = 'pointer';
-
-        // Center the bunny's anchor point
-        bunny.anchor.set(0.5);
-
-        // Make it a bit bigger, so it's easier to grab
-        bunny.scale.set(3);
-
-        // Setup events for mouse + touch using the pointer events
-        bunny.on('pointerdown', onDragStart, bunny);
-
-        // Move the sprite to its designated position
-        bunny.x = x;
-        bunny.y = y;
-
-        // Add it to the stage
-        app.stage.addChild(bunny);
-    }
-
-    let dragTarget = null;
-
-    app.stage.eventMode = 'static';
-    app.stage.hitArea = app.screen;
-    app.stage.on('pointerup', onDragEnd);
-    app.stage.on('pointerupoutside', onDragEnd);
-
-    function onDragMove(event) {
-        if (dragTarget) {
-            dragTarget.parent.toLocal(event.global, null, dragTarget.position);
-        }
-    }
-
-    function onDragStart() {
-        // Store a reference to the data
-        // * The reason for this is because of multitouch *
-        // * We want to track the movement of this particular touch *
-        this.alpha = 0.5;
-        dragTarget = this;
-        app.stage.on('pointermove', onDragMove);
-    }
-
-    function onDragEnd() {
-        if (dragTarget) {
-            app.stage.off('pointermove', onDragMove);
-            dragTarget.alpha = 1;
-            dragTarget = null;
-        }
-    }
+  app.stage.addChild(circle);
 })();
